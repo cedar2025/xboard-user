a6358590ecad0685bb7c6233d1782cc91a7ac871
GitHub Action <xboard@xboard.one>
2024-03-19 18:07:24 +0000

    fix: 修复token2Login在已登陆情况下无法跳转参数指定路由的问题 修复登陆成功后无法跳转指定页面的问题 修复前端语言与后台返回语言不通不的问题 调整邀请码列表样式（增加边框）

---
+++ env.example.js
@@ -0,0 +1,15 @@
+// API地址
+window.routerBase = 'http://127.0.0.1:8000/'
+window.settings = {
+  // 站点名称
+  title: 'Xboard',
+  // 站点描述
+  description: 'Xboard',
+  assets_path: '/assets',
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: '',
+  // 站点LOGO
+  logo: '',
+}
